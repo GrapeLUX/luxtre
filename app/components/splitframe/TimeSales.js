a1b6b96d4dd42cecba33616c5eedfbacd98b526b
sarawut <sanit.sa@outlook.com>
2018-11-22 04:45:17 -0800

    Time&Sales

---
+++ app/components/splitframe/TimeSales.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+
+type Props = {
+    coinPrice: number,
+  };  
+
+@observer
+export default class OrderBook extends Component<Props> {
+
+    render() {
+        return (
+            <div>
+                Here is OrderBook Page
+            </div>   
+        );
+    }
+}
