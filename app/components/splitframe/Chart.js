8fe6905238b1af35998b1cb8f252cb54e919a888
sarawut <sanit.sa@outlook.com>
2018-11-27 07:47:39 -0800

    Chart Function

--- app/components/splitframe/Chart.js
+++ app/components/splitframe/Chart.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
+import { render } from 'react-dom';
+import Chart from './Chart/Chart';
+import { getData } from "./Chart/utils"
+
+import { TypeChooser } from "react-stockcharts/lib/helper";
 
 type Props = {
     coinPrice: number,
   };
 
 @observer
-export default class Chart extends Component<Props> {
+export default class ChartComponent extends Component<Props> {
+    componentDidMount() {
+		getData().then(data => {
+			this.setState({ data })
+		})
+	}
 
     render() {
-        return (
-            <div>
-                Here is Chart Page
-            </div>   
-        );
+        if (this.state == null) {
+			return <div>Loading...</div>
+		}
+		return (
+			<TypeChooser>
+				{type => <Chart type={type} data={this.state.data} />}
+			</TypeChooser>
+		)
     }
 }
+
+render(
+	<ChartComponent />,
+	document.getElementById("root")
+);
 
 
