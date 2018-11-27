258df6c404f41058a00b64577fa0a33e3d8b1a3d
sarawut <sanit.sa@outlook.com>
2018-11-27 08:57:10 -0800

    Add Header on OrderBook

--- app/components/splitframe/OrderBook.js
+++ app/components/splitframe/OrderBook.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
+import styles from './OrderBook.scss';
 
 type Props = {
     coinPrice: number,
@@ -10,8 +11,13 @@ export default class OrderBook extends Component<Props> {
 
     render() {
         return (
-            <div>
-                Here is OrderBook Page
+            <div className={styles.component}>
+                <div className={styles.HeaderContainer}>
+                    <div className={styles.Header_Proportion}>Proportion</div>
+                    <div className={styles.Header_Size}>Size</div>
+                    <div className={styles.Header_Price}>Price</div>
+                </div>
+                
             </div>   
         );
     }
