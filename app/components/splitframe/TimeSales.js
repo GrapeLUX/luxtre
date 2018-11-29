e77721ba8567611b5a5edf5c559cf9b6ac7d3020
sarawut <sanit.sa@outlook.com>
2018-11-29 04:47:58 -0800

    Add Header on Time&Sales

--- app/components/splitframe/TimeSales.js
+++ app/components/splitframe/TimeSales.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
+import styles from './TimeSales.scss'
 
 type Props = {
     coinPrice: number,
@@ -10,8 +11,12 @@ export default class TimeSales extends Component<Props> {
 
     render() {
         return (
-            <div>
-                Here is Time and Sales Page
+            <div className={styles.component}>
+                <div className={styles.HeaderContainer}>
+                    <div className={styles.Header_Time}>Time</div>
+                    <div className={styles.Header_Price}>Price</div>
+                    <div className={styles.Header_Size}>Size</div>
+                </div>
             </div>   
         );
     }
